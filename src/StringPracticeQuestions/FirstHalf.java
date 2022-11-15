0193db542710a1663514bf130155639e74fa517a
KaregaCarrington <karega.carrington@gmail.com>
2022-11-15 15:30:50 -0500

    First commit

---
+++ src/StringPracticeQuestions/FirstHalf.java
@@ -0,0 +1,16 @@
+package StringPracticeQuestions;
+
+    public class FirstHalf {
+    //    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+    //
+    //
+    //    firstHalf("WooHoo") → "Woo"
+    //    firstHalf("HelloThere") → "Hello"
+    //    firstHalf("abcdef") → "abc"
+
+    }
+
+    public String firstHalf() {
+
+    }
+
